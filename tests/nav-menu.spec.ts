d880f3121b197c8f7cd2f032120739caf72de80e
Arpik Sargsyan <arpiksargsyan@gmail.com>
2024-06-25 17:33:27 +0400

    Add tests for nav bar

---
+++ tests/nav-menu.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Check API page', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
+    await page.getByRole('link', { name: 'API' }).click();
+    await expect(page).toHaveURL(/.*class-playwright/)
+    await expect(page).toHaveTitle(/Playwright Library/)
+  });
+
+  test('Check Community page', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
+    await page.getByRole('link', { name: 'Community' }).click();
+    await expect(page).toHaveURL(/.*welcome/)
+    await expect(page).toHaveTitle(/Welcome/)
+  })
